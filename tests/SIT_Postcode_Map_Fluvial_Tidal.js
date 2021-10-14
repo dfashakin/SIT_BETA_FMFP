03bea8a909d003a6f1aeb2f442b0a3febbf27830
David Fashakin <44804122+dfashakin@users.noreply.github.com>
2021-10-14 07:51:32 +0100

    Update SIT_Postcode_Map_Fluvial_Tidal.js

--- tests/SIT_Postcode_Map_Fluvial_Tidal.js
+++ tests/SIT_Postcode_Map_Fluvial_Tidal.js
@@ -44,7 +44,6 @@ describe('Validate that the Application Complete Page is Displayed', () => {
     });
 
 
-
 	
 
 	it('Validate that the Accessibility footer Link', () => {
@@ -60,19 +59,19 @@ describe('Validate that the Application Complete Page is Displayed', () => {
 
 
 
+
 		it('Validate that the Terms and conditions Link is Displayed', () => {
 			const termsAndConditionsLink = SIT_Login.termsAndConditionsLink;
 			 expect(termsAndConditionsLink).toBeDisplayed();
 			});	
 
-	
+
 		
 		it('Validate that the Privacy Notice Link is Displayed', () => {
 			const privacyNoticeLink = SIT_Login.privacyNoticeLink;
 			expect(privacyNoticeLink).toBeDisplayed();
 		});
-	
-	
+
 
 		it('Validate that the Built by the Environment Agency Link is Displayed', () => {
 			const builtByEnvironmentAgencyLink = SIT_Login.builtByEnvironmentAgencyLink;
@@ -102,22 +101,21 @@ describe('Validate that the Application Complete Page is Displayed', () => {
       });
 
 
-    it('Validate that the correct page is displayed if the contact the environment agency Link is selected', () => {
-        const contactEnvironmentAgencyLink = SIT_Application_Complete.contactEnvironmentAgencyLink;
-        contactEnvironmentAgencyLink.waitForExist();
-        contactEnvironmentAgencyLink.click();
-        browser.pause(2000);
-        //expect(browser).toHaveUrl('https://www.gov.uk/guidance/flood-risk-assessment-for-planning-applications#contact');
-        //expect(browser).toHaveTitle("Flood risk assessments if you're applying for planning permission - GOV.UK");
-		expect(browser).toHaveUrl('https://www.gov.uk/contact');
-        expect(browser).toHaveTitle("Find contact details for services - GOV.UK");
-        browser.pause(5000);
-        browser.back();
-      });
+
 
 
 	
 });
+
+
+
+
+
+
+
+
+    
+
 
 
 
