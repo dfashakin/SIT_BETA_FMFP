3489790a19c02c208a8ff23a38f07277e652fbc1
David Fashakin <44804122+dfashakin@users.noreply.github.com>
2021-10-14 04:33:37 +0100

    Update SIT_Postcode_No_Historic_Defence_Modelled.js

--- tests/SIT_Postcode_No_Historic_Defence_Modelled.js
+++ tests/SIT_Postcode_No_Historic_Defence_Modelled.js
@@ -43,12 +43,7 @@ describe('Validate that the Application Complete Page is Displayed', () => {
         
     });
 
-    it('Validate that the Help Footer Link is Displayed', () => {
-		const helpFooterLink = SIT_Login.helpFooterLink;
-		helpFooterLink.waitForExist();
-		expect(helpFooterLink).toBeDisplayed();
-	
-	});
+
 
 	
 
@@ -63,10 +58,7 @@ describe('Validate that the Application Complete Page is Displayed', () => {
 	 expect(cookiesFooterLink).toBeDisplayed();
 	});
 
-	it('Validate that the Privacy Policy Link is Displayed', () => {
-		const privacyPolicyLink = SIT_Login.privacyPolicyLinkDisplay;
-		 expect(privacyPolicyLink).toBeDisplayed();
-		});
+
 
 
 		it('Validate that the Terms and conditions Link is Displayed', () => {
@@ -74,20 +66,13 @@ describe('Validate that the Application Complete Page is Displayed', () => {
 			 expect(termsAndConditionsLink).toBeDisplayed();
 			});	
 
-		it('Validate that the Contact Link is Displayed', () => {
-		const contactLink = SIT_Login.contactLink;
-		expect(contactLink).toBeDisplayed();
-		});
 		
 		it('Validate that the Privacy Notice Link is Displayed', () => {
 			const privacyNoticeLink = SIT_Login.privacyNoticeLink;
 			expect(privacyNoticeLink).toBeDisplayed();
 		});
 	
-		it('Validate that the CymraegLink is Displayed', () => {
-			const cymraegLink = SIT_Login.cymraegLink;
-			expect(cymraegLink).toBeDisplayed();
-		});
+	
 
 		it('Validate that the Built by the Environment Agency Link is Displayed', () => {
 			const builtByEnvironmentAgencyLink = SIT_Login.builtByEnvironmentAgencyLink;
